2340b23dc2b1370e69a0bd225caa6d78994b1685
Riddhi Zantye <riddhizantye@Riddhis-MacBook-Air.local>
2024-02-07 18:09:03 +0530

    Eigenval, vec and maper added

--- max_plus/functions.py
+++ max_plus/functions.py
@@ -1,6 +1,3 @@
-#  I am going to create a matrix module from scratch which will input number of rows and columns as well as the elements of the matrix.
-# I will then perform operations on the matrix
-
 # E = float('-inf')
 
 from multimethod import multimethod 
@@ -8,6 +5,10 @@ from multimethod import multimethod
 # matrix class in python
 class Matrix:
     def __init__(self, string, zeros = 0, rows = 0, columns = 0):   
+        self.cycles = []
+        self.critical_cycles = []
+        # initialize empty eigenvalue
+        self.eigen = float('-inf')
         if zeros == 1:
             self.rows = rows
             self.columns = columns
@@ -21,103 +22,125 @@ class Matrix:
             return
 
         else:
-            # remove the first and last character
             string = string[1:-1]
             # print(string)
-            # split the string by comma
             string = string.split(",")
             # print(string)
-            # create an empty matrix as a variable of the class
             self.matrix = []
-            # create empty list
-            # rows = []
-            # print(len(string))
-            # define private varialbes for number of rows and columns and find them
+
             self.rows = len(string)
             self.columns = len(string[0].split(" "))
             for row in string:
-                # remove leading spaces
+                # removes leading spaces
                 row = row.lstrip()
-                # remove the first and last character
                 row = row[1:-1]
-                # # split the string by space
                 row = row.split(" ")
                 # print(len(row))
                 # print(row)
-                # # convert the string to a float
+                # converts E to float('-inf')
+                for i in range(len(row)):
+                    if row[i] == "E":
+                        row[i] = float('-inf')
                 row = [float(i) for i in row]
                 # print(row)
-                # # append the row to the matrix
                 self.matrix.append(row)
         # print(self.matrix) 
+                
+    def find_cycles(self):
+        import networkx as nx
+        edges = []
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[i])):
+                if self.matrix[i][j] != float('-inf'):
+                    edges.append((i, j))
+        # makes digraph using the edges
+        G = nx.DiGraph(edges)
+
+        self.cycles = list(nx.simple_cycles(G))
+
+    def find_critical_cycles(self):
+        maxi = float('-inf')
+        for i in range(len(self.cycles)):
+            temp = 0
+            for j in range(len(self.cycles[i])):
+                temp += self.matrix[self.cycles[i][j]][self.cycles[i][(j+1)%len(self.cycles[i])]]
+            
+            if temp/len(self.cycles[i]) > maxi:
+                maxi = temp/len(self.cycles[i])
+                self.critical_cycles.clear()
+                self.critical_cycles.append(self.cycles[i])
+            elif temp/len(self.cycles[i]) == maxi:
+                self.critical_cycles.append(self.cycles[i])
+
+        return maxi
     
-    # def print_matrix(self):
-    #     # for loop to iterate through the rows
-    #     for i in range(self.__rows):
-    #         # for loop to iterate through the columns
-    #         for j in range(self.__columns):
-    #             # print the element
-    #             print(self.matrix[i][j], end = " ")
-    #         # print a new line
-    #         print()
+    def eigenval(self):
+        # checks if eigenvalue(self.eigen) is already present
+        if(self.eigen != float('-inf')):
+            return self.eigen
+        if(self.cycles == []):
+            self.find_cycles()
+        # print(self.cycles)
+        if(self.critical_cycles == []):
+            self.eigen = self.find_critical_cycles()
+
+        # print(self.critical_cycles)
+        return self.eigen
     
-     
+    def eigenvec(self):
+        # checks if eigenvalue(self.eigen) is already present
+        if(self.eigen == float('-inf')):
+            self.eigen = self.eigenval()
+
+        a = Matrix("", 2, self.rows, self.columns)
+        if(self.eigen > 0):
+            a = subtract(self, self.eigen)
+
+        # stores gamma
+        g = gamma(a)
+        # print_matrix(g)
+        cols = []
+
+        for i in range(len(self.critical_cycles)):
+            temp = []
+            for j in range(len(g.matrix)):
+                temp.append(g.matrix[j][self.critical_cycles[i][0]])
+            cols.append(temp)
+
+        cols = find_distinct(cols)
+        return cols
+       
     
 def print_matrix(a):
-    # for loop to iterate through the rows
     for i in range(a.rows):
-        # for loop to iterate through the columns
         for j in range(a.columns):
-            # print the element
             print(a.matrix[i][j], end = " ")
-        # print a new line
         print()
     
-"""
-a = Matrix(2, 2)
-2 
-b = Matrix(2, 2)
-5 2
-3 8
-Addition
-c = add(a, b)
-5 6
-7 8
-"""
 # function to add two elements
 def add_elements(a, b):
     return round(max(a, b), 6)
 
 # function to multiply two elements
 def multiply_elements(a, b):
-    # if a == e or b == e:
-    #     return e
     return round(a + b, 6)
 
 # function to add two matrices
+@multimethod
 def add(a: Matrix, b: Matrix):
-    # check if the dimensions of the matrices are equal
     if a.rows != b.rows or a.columns != b.columns:
         raise ValueError("The dimensions of the matrices are not equal")
-    # create an empty matrix
+    # creates an empty matrix
     c = Matrix("", 1, a.rows, a.columns)
-    # for loop to iterate through the rows
+    
     for i in range(a.rows):
-        # for loop to iterate through the columns
         for j in range(a.columns):
-            # add the elements
             c.matrix[i][j] = add_elements(a.matrix[i][j], b.matrix[i][j])
     return c
-
-# a = Matrix(2, 2)
-# b = Matrix(2, 2)
-# c = add(a, b)
-# print_matrix(c)
 
 # function to multiply two matrices
 @multimethod
 def multiply(a: Matrix, b: Matrix):
-    # check if the number of columns of a is equal to the number of rows of b
     if a.columns != b.rows:
         raise ValueError("The number of columns of a is not equal to the number of rows of b")
     # create an empty matrix
@@ -133,23 +156,21 @@ def multiply(a: Matrix, b: Matrix):
 
 @multimethod
 def multiply(a: int, b: Matrix):
-    # create an empty matrix
+    # creates an empty matrix
     c = Matrix("", True, b.rows, b.columns)
-    # for loop to iterate through the rows
     for i in range(b.rows):
-        # for loop to iterate through the columns
         for j in range(b.columns):
             c.matrix[i][j] = multiply_elements(a, b.matrix[i][j])
     return c
 
+@multimethod
 def subtract(a: Matrix, b: float):
     # create an empty matrix
     c = Matrix("", True, a.rows, a.columns)
-    # for loop to iterate through the rows
     for i in range(a.rows):
-        # for loop to iterate through the columns
         for j in range(a.columns):
             c.matrix[i][j] = a.matrix[i][j] - b
+
     return c
 
 # write a function to find the power of a matrix
@@ -157,17 +178,8 @@ def power(a, n):
     # check if the matrix is square
     if a.rows != a.columns:
         raise ValueError("The matrix is not square")
-    # create a new matrix
-    # c = Matrix("", True, a.rows, a.columns)
     c = a
-    # # initialize c as identity matrix
-    # for i in range(a.rows):
-    #     for j in range(a.columns):
-    #         if i == j:
-    #             c.matrix[i][j] = 1
-    #         else:
-    #             c.matrix[i][j] = 0
-    # multiply the matrix n times
+    # multiply the matrix n-1 times
     for i in range(n-1):
         c = multiply(c, a)
     return c
@@ -183,11 +195,12 @@ def maxEl(a: Matrix):
                 maxi = a.matrix[i][j]
     return maxi
 
+# @multimethod
 def exp(a: Matrix):
     ans = subtract(a, 1)
     tempMat = a
     t = maxEl(a)
-    # check if t is float or int
+    # checks if t is float or int
     if t.is_integer():
         t = int(t)
     else:
@@ -203,6 +216,7 @@ def exp(a: Matrix):
 
     return ans
 
+
 def gamma(a : Matrix):
     tempMat = a
     ans = a
@@ -212,34 +226,87 @@ def gamma(a : Matrix):
         ans = add(ans, tempMat)
     return ans
 
-"""
-a = Matrix(2, 2)
-1 6
-7 4
-b = Matrix(2, 2)
-5 2
-3 8
-Multiplication
-c = multiply(a, b)
-9 14
-12 12
-"""
+@multimethod
+def subtract(a: list, b: list):
+    # create an empty matrix
+    c = []
 
-a = Matrix("[[7 9 5 5 3 7], [7 5 2 7 0 4], [8 0 3 3 8 0], [7 2 5 7 5 9], [4 2 6 6 8 8], [3 0 5 7 1 2]]")
-# a = Matrix("[[1 2 3], []]")
+    for i in range(len(a)):
+        c.append(a[i] - b[i])
+    return c
+
+def find_distinct(cols):
+    # print("Original cols:")
+    # print(cols)
+    remove = []
+    n = len(cols)
+    m = len(cols[0])
+    # traverse cols. check if the index is not in remove
+    for i in range(n):
+        if i not in remove:
+            for j in range(i+1, n):
+                c = subtract(cols[i], cols[j])
+                val = c[0]
+                allsame = True
+                for k in range(1, m):
+                    if c[k] != val:
+                        allsame = False
+                        break
+                if allsame:
+                    remove.append(j)
+    
+    for i in sorted(remove, reverse=True):
+        del cols[i]
+
+    return cols
+
+def maper(a: Matrix):
+    # traverse through the matrix, takes one value from each rown and column and add them and
+    # stores the combination with the maximum value
+    import dlib   
+
+    b = []
+    for i in range(a.rows):
+        temp = []
+        for j in range(a.columns):
+            # add the elements
+            if(a.matrix[i][j] == float('-inf')):
+                temp.append(-1e15)
+            else:
+                temp.append(a.matrix[i][j])
+        b.append(temp)
+
+    assignment = dlib.max_cost_assignment(dlib.matrix(b))
+    # print(assignment)
+    sum = 0
+    for i in range(len(assignment)):
+        sum += a.matrix[i][assignment[i]]
+
+    return sum
+
+a = Matrix("[[4 4 3 8 1], [3 3 4 5 4], [5 3 4 7 3], [2 1 2 3 0], [6 6 4 8 1]]")
+# b = Matrix("[[18 17 16 16 15], [17 18 16 16 15], [15 15 18 18 17], [15 15 18 18 17], [16 16 19 19 18]]")
+# a = Matrix("[[1 2 3]]")
+# a = Matrix("[[2 4 2], [4 3 1], [2 1 3]]")
+# a = Matrix("[[-3 -2 8], [1 0 4], [2 5 -6]]")
+# a = Matrix("[[0 3 E E], [1 -1 E E], [E E 2 E], [E E E 1]]")
+# a = Matrix("[[2 0 -1 3 1], [3 -1 1 2 0], [0 4 -1 2 1], [1 2 2 1 0], [-1 0 1 0 0]]")
+# a = Matrix("[[3 6], [2 1]]")
 # print_matrix(a)
-# a = Matrix("[[-5 2], [-3 8]]")
 # b = Matrix(2, 2)
-# c = multiply(a, a)
+# c = multiply(a, b)
+# print_matrix(c)
 # c = power(a, 2)
 
-# c = gamma(a)
-b = subtract(a, 8)
-c = gamma(b)
-print_matrix(c)
+# c = a.eigenval()
+# print("Eigenvalue is", c)
+# d = a.eigenvec()
+# print(d)
+# a = 5 
+# 3.66
+# e = log(a)
+# e = maper(a)
+# print(e)
 
-"""
-Doubts
-- round
-- eigenvalue, eigenvector
-"""
+# print(ev)
+# print(c)
